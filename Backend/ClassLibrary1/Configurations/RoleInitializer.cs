c58b7cd4ffcd466f3b64466b3beb18ce9bcf2c28
YuvelirSasha <75026042+YuvelirSasha@users.noreply.github.com>
2022-05-07 19:10:14 +0300

    changed method created admin and roles

---
+++ Backend/ClassLibrary1/Configurations/RoleInitializer.cs
@@ -0,0 +1,46 @@
+﻿using DataAccess.Models;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Configurations
+{
+    public class RoleInitializer
+    {
+        public static async Task InitializeAsync(UserManager<User> userManager, RoleManager<IdentityRole<int>> roleManager)
+        {
+            string adminEmail = "kripaknomer1@gmail.com";
+            string password = "string";
+
+            if(await roleManager.FindByNameAsync("admin") == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole<int>("admin"));
+                await roleManager.CreateAsync(new IdentityRole<int>("student"));
+            }
+
+            if(await userManager.FindByNameAsync(adminEmail) == null)
+            {
+                User admin = new User
+                {
+                    FirstName = "Sasha",
+                    LastName = "WAWAWA",
+                    UserName = "WAWAWA",
+                    Email = "kripaknomer1@gmail.com",
+                    NormalizedEmail = "KRIPAKNOMER1@GMAIL.COM",
+                    RegistrationDate = DateTime.UtcNow,
+                    EmailConfirmed = true
+                };
+                IdentityResult result = await userManager.CreateAsync(admin, password);
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(admin, "admin");
+                }
+            }
+        }
+
+
+    }
+}
